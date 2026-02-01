becaa71642fa14ec8091a68912e26d1c56418ad7
sj-jason-liu <sj.jason.liu@gmail.com>
2026-02-01 15:32:01 +0800

    chore: Configure build output to docs for GitHub Pages / 設定建置輸出至 docs 以修復部署

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "",
   build: {
+    outDir: "docs",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
